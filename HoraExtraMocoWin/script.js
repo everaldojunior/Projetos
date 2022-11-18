6c72999d467cf66071ff7f9abb0c5819969f3b03
Everaldo <sdbarros1@gmail.com>
2022-11-18 18:19:08 -0300

    Novo JS

--- HoraExtraMocoWin/script.js
+++ HoraExtraMocoWin/script.js
@@ -1,9 +1,30 @@
-function calcPct (x, y){
+function calcularImovel(m, q){
+    m2 = 3000;
+    vm = m * m2;
     
-    (x / y) * 100;
-    
+    switch (q){
+        case 1:
+            q1 = m2 * 1;
+            break;    
+        
+        case 2:
+            q1 = m2 * 1.2;
+            break;    
 
-    console.log("Você tem " + resultado + "h de hora extra" );
+        case 3:
+           q1 = m2 * 1.5;
+            break;    
+    }
+    
+    return  vm * q1;
+
+    console.log(q);
+    console.log(m2);
+    console.log(vm);
 }
 
-horaExtra(8, 12, 14, 18)
+
+let metragem = 123;
+let quartos = 3;
+let preco = calcularImovel(metragem, quartos);
+console.log('A casa custa R$' + preco);
