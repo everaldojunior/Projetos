a1adb1eb2732f5d0a714ef4e34c31c1aca691b11
Everaldo <sdbarros1@gmail.com>
2022-12-30 08:16:49 -0300

    Novo projeto JrFlix

--- HoraExtraMocoWin/script.js
+++ HoraExtraMocoWin/script.js
@@ -1,30 +1,24 @@
 function calcularImovel(m, q){
-    m2 = 3000;
-    vm = m * m2;
-    
+    let m2 = 3.000;
+    let preco = 0;
     switch (q){
         case 1:
-            q1 = m2 * 1;
-            break;    
+        default:
+           preco = m * (m2 * 1);
+            break;
         
         case 2:
-            q1 = m2 * 1.2;
-            break;    
-
+            preco = m * (m2 * 1.2);
+            break;
+        
         case 3:
-           q1 = m2 * 1.5;
-            break;    
-    }
-    
-    return  vm * q1;
-
-    console.log(q);
-    console.log(m2);
-    console.log(vm);
+            preco = m * (m2 * 1.5);
+            break;
+    }     
+    return preco;
 }
 
-
-let metragem = 123;
-let quartos = 3;
+let metragem = 200;
+let quartos = 2;
 let preco = calcularImovel(metragem, quartos);
-console.log('A casa custa R$' + preco);
+console.log(`A casa custa R$ ${preco}`);
